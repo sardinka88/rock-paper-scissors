507898108ececd925776515977211b6432903448
Srdan <118986032+sardinka88@users.noreply.github.com>
2024-01-02 12:36:16 +0100

    changed logic and ui removed game() function, rounds will now be played one by one there will be no round number setting user will play individual rounds to do - connect with UI, play to 5 wins

--- script.js
+++ script.js
@@ -6,61 +6,66 @@ function getComputerChoice() {
   return actionWords[Math.floor(Math.random() * actionWords.length)];
 }
 
-const setRounds = () => {
-  let rounds = document.querySelector("#input-rounds").value;
-  console.log(rounds);
-  let roundsDiv = document.querySelector(".div.rounds");
-  const roundsMessage = document.createElement("p");
-  roundsMessage.textContent = `You are playing ${rounds} rounds.`;
-  roundsDiv.appendChild(roundsMessage);
+const playButton = document.querySelector(".button.rounds");
+let playerSelection = "";
+
+playButton.addEventListener("click", () => {
+  playRound(playerSelection, getComputerChoice());
+});
+
+const getSelection = (event) => {
+  playerSelection = event.target.textContent;
 };
 
-const playButton = document.querySelector(".button.rounds");
-
-playButton.addEventListener("click", setRounds);
+let actionButtons = document.querySelectorAll(".actionButton");
+actionButtons.forEach((button) => {
+  button.addEventListener("click", getSelection);
+});
 
 // function that plays one round of RPS game
 // returns true if player wins, false if computer wins
 // in case of a tie, it returns a string "tie"
 function playRound(playerSelection, computerSelection) {
+  console.log(playerSelection);
+  console.log(computerSelection);
   if (playerSelection.toLowerCase() === "rock") {
     switch (computerSelection) {
       case "rock":
-        return "tie";
+        console.log("tie");
         break;
       case "paper":
-        return false;
+        console.log("You win");
         break;
       case "scissors":
-        return true;
+        console.log("You lose");
         break;
     }
   } else if (playerSelection.toLowerCase() === "paper") {
     switch (computerSelection) {
       case "rock":
-        return true;
+        console.log("You win");
         break;
       case "paper":
-        return "tie";
+        console.log("tie");
         break;
       case "scissors":
-        return false;
+        console.log("You lose");
         break;
     }
   } else if (playerSelection.toLowerCase() === "scissors") {
     switch (computerSelection) {
       case "rock":
-        return false;
+        console.log("You lose");
         break;
       case "paper":
-        return true;
+        console.log("You win");
         break;
       case "scissors":
-        return "tie";
+        console.log("tie");
         break;
     }
   } else {
-    return "Please provide a valid selection!";
+    console.log("Please provide a valid selection!");
   }
 }
 
@@ -81,30 +86,32 @@ function checkWinner(score1, score2) {
 // logs score for each round, and final score
 // for testing purposes, will log result of reach round
 // should improve with logic that handles non-compliant inputs of number of rounds
-function game(numberOfRounds) {
-  let playerScore = 0;
-  let computerScore = 0;
-  let result;
-  let playerSelection;
-  let computerSelection;
-  for (let i = 1; i <= numberOfRounds; i++) {
-    playerSelection = prompt("Choose rock, paper or scissors: ", "");
-    computerSelection = getComputerChoice();
-    console.log(
-      `You chose ${playerSelection.toLowerCase()}, the computer chose ${computerSelection}`
-    );
-    result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result === true) {
-      playerScore++;
-    } else if (result === false) {
-      computerScore++;
-    } else if (result === "Please provide a valid selection!") {
-      i--;
-    }
+// function game(numberOfRounds) {
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   let currentRound = 0;
+//   let result;
+//   let playerSelection;
+//   let computerSelection;
+//   for (let i = 1; i <= numberOfRounds; i++) {
+//     playerSelection = getSelection;
+//     computerSelection = getComputerChoice();
+//     currentRound = i;
+//     console.log(
+//       `You chose ${playerSelection}, the computer chose ${computerSelection}`
+//     );
+//     result = playRound(playerSelection, computerSelection);
+//     console.log(result);
+//     if (result === true) {
+//       playerScore++;
+//     } else if (result === false) {
+//       computerScore++;
+//     } else if (result === "Please provide a valid selection!") {
+//       i--;
+//     }
 
-    console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-  }
-  console.log(checkWinner(playerScore, computerScore));
-  return `Final score is: Player ${playerScore} - Computer ${computerScore}`;
-}
+//     console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+//   }
+//   console.log(checkWinner(playerScore, computerScore));
+//   return `Final score is: Player ${playerScore} - Computer ${computerScore}`;
+// }
